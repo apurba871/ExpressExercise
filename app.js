a97bbd47811f66ed918c766e4f65215b9326f4b5
apurba_debnath <apurba871@gmail.com>
2020-12-22 09:08:21 +0530

    Removed redundant codes

--- app.js
+++ app.js
@@ -7,28 +7,40 @@ app.get("/", function(req, res) {
 	res.send("Hi there, welcome to my assignment!");
 });
 
-app.get("/speak/pig", function(req, res) {
-	res.send("The pig says 'Oink'");
+app.get("/speak/:animal", function(req, res) {
+	var animal = req.params.animal;
+	var sound, flag = 0;
+	if (animal === "pig")
+		sound = "Oink";
+	else if (animal === "cow")
+		sound = "Moo";
+	else if (animal === "dog")
+		sound = "Woof Woof";
+	else if (animal === "cat")
+		sound = "Meow"
+	else if (animal === "sheep")
+		sound = "Mehahah"
+	else
+		flag = 1;
+	if (!flag)
+		res.send("The " + animal + " says '" + sound + "'");
+	else
+		res.send("Animal not found");
 });
 
-app.get("/speak/cow", function(req, res) {
-	res.send("The cow says 'Moo'");
-});
-
-app.get("/speak/dog", function(req, res) {
-	res.send("The dog says 'Woof Woof!'");
-});
-
-app.get("/repeat/hello/3", function(req, res) {
-	res.send("hello hello hello");
-});
-
-app.get("/repeat/hello/5", function(req, res) {
-	res.send("hello hello hello hello hello");
-});
-
-app.get("/repeat/blah/2", function(req, res) {
-	res.send("blah blah");
+app.get("/repeat/:word/:times", function(req, res) {
+	var word = req.params.word;
+	var times = Number(req.params.times);
+	var str;
+	if (times > 0) {
+		str = word;
+		for (var i = 1; i < times; ++i) {
+			str += " " + word;
+		}
+		res.send(str);
+	} else {
+		res.send();
+	}
 });
 
 app.get("*", function(req, res) {
