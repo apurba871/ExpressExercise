8359250b9a83659cb6990570db38d719145fb9d8
apurba_debnath <apurba871@gmail.com>
2020-12-22 08:27:21 +0530

    Added speak routes

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.get("/", function(req, res) {
 	res.send("Hi there, welcome to my assignment!");
 });
 
+app.get("/speak/pig", function(req, res) {
+	res.send("The pig says 'Oink'");
+});
+
+app.get("/speak/cow", function(req, res) {
+	res.send("The cow says 'Moo'");
+});
+
+app.get("/speak/dog", function(req, res) {
+	res.send("The dog says 'Woof Woof!'");
+});
+
 app.listen(3000, function() {
 	console.log("Server started successfully, listening app on port 3000");
 });
